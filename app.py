add97042bc8d185421e794c2447c8a465803e196
devssk <sksong419@naver.com>
2022-01-12 14:03:52 +0900

    route '/' def home()에 있던 크롤링을 따로 빼서 함수로 묶음 -> eplinfo() eplinfo()에 팀링크도 추가 기존에 상세페이지로 가던 site_list 교체 -> 이제 순위에 따라 바뀌어도 팀명 그대로 따라감

--- app.py
+++ app.py
@@ -18,31 +18,80 @@ SECRET_KEY = 'SPARTA'
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-site_list = ['https://www.goal.com/kr/%ED%8C%80/%EB%A7%A8%EC%B2%B4%EC%8A%A4%ED%84%B0-%EC%8B%9C%ED%8B%B0/a3nyxabgsqlnqfkeg41m6tnpp',
-             'https://www.goal.com/kr/%ED%8C%80/%EC%B2%BC%EC%8B%9C/9q0arba2kbnywth8bkxlhgmdr',
-             'https://www.goal.com/kr/%ED%8C%80/%EB%A6%AC%EB%B2%84%ED%92%80/c8h9bw1l82s06h77xxrelzhur',
-             'https://www.goal.com/kr/%ED%8C%80/%EC%95%84%EC%8A%A4%EB%84%90/4dsgumo7d4zupm2ugsvm4zm4d',
-             'https://www.goal.com/kr/%ED%8C%80/%EC%9B%A8%EC%8A%A4%ED%8A%B8%ED%96%84-%EC%9C%A0%EB%82%98%EC%9D%B4%ED%8B%B0%EB%93%9C/4txjdaqveermfryvbfrr4taf7',
-             'https://www.goal.com/kr/%ED%8C%80/%ED%86%A0%ED%8A%B8%EB%84%98-%ED%99%8B%EC%8A%A4%ED%8D%BC/22doj4sgsocqpxw45h607udje',
-             'https://www.goal.com/kr/%ED%8C%80/%EB%A7%A8%EC%B2%B4%EC%8A%A4%ED%84%B0-%EC%9C%A0%EB%82%98%EC%9D%B4%ED%8B%B0%EB%93%9C/6eqit8ye8aomdsrrq0hk3v7gh',
-             'https://www.goal.com/kr/%ED%8C%80/%EC%9A%B8%EB%B2%84%ED%96%84%ED%8A%BC-%EC%9B%90%EB%8D%94%EB%9F%AC%EC%8A%A4/b9si1jn1lfxfund69e9ogcu2n',
-             'https://www.goal.com/kr/%ED%8C%80/%EB%B8%8C%EB%9D%BC%EC%9D%B4%ED%8A%BC-%EC%95%A4-%ED%98%B8%EB%B8%8C/e5p0ehyguld7egzhiedpdnc3w',
-             'https://www.goal.com/kr/%ED%8C%80/%EB%A0%88%EC%8A%A4%ED%84%B0-%EC%8B%9C%ED%8B%B0/avxknfz4f6ob0rv9dbnxdzde0',
-             'https://www.goal.com/kr/%ED%8C%80/%EC%82%AC%EC%9A%B0%EC%83%98%ED%94%84%ED%84%B4/d5ydtvt96bv7fq04yqm2w2632',
-             'https://www.goal.com/kr/%ED%8C%80/%ED%81%AC%EB%A6%AC%EC%8A%A4%ED%84%B8-%ED%8C%B0%EB%A6%AC%EC%8A%A4/1c8m2ko0wxq1asfkuykurdr0y',
-             'https://www.goal.com/kr/%ED%8C%80/%EB%B8%8C%EB%A0%8C%ED%8A%B8%ED%8F%AC%EB%93%9C/7yx5dqhhphyvfisohikodajhv',
-             'https://www.goal.com/kr/%ED%8C%80/%EC%95%84%EC%8A%A4%ED%86%A4-%EB%B9%8C%EB%9D%BC/b496gs285it6bheuikox6z9mj',
-             'https://www.goal.com/kr/%ED%8C%80/%EC%97%90%EB%B2%84%ED%8A%BC/ehd2iemqmschhj2ec0vayztzz',
-             'https://www.goal.com/kr/%ED%8C%80/%EB%A6%AC%EC%A6%88-%EC%9C%A0%EB%82%98%EC%9D%B4%ED%8B%B0%EB%93%9C/48gk2hpqtsl6p9sx9kjhaydq4',
-             'https://www.goal.com/kr/%ED%8C%80/%EC%99%93%ED%8D%BC%EB%93%9C/4t83rqbdbekinxl5fz2ygsyta',
-             'https://www.goal.com/kr/%ED%8C%80/%EB%B2%88%EB%A6%AC/64bxxwu2mv2qqlv0monbkj1om',
-             'https://www.goal.com/kr/%ED%8C%80/%EB%89%B4%EC%BA%90%EC%8A%AC-%EC%9C%A0%EB%82%98%EC%9D%B4%ED%8B%B0%EB%93%9C/7vn2i2kd35zuetw6b38gw9jsz',
-             'https://www.goal.com/kr/%ED%8C%80/%EB%85%B8%EB%A6%AC%EC%B9%98-%EC%8B%9C%ED%8B%B0/suz80crpy3anixyzccmu6jzp',]
+#---------골닷컴사이트에서 epl정보 크롤링함수
+def eplinfo():
+    url = 'https://www.goal.com/kr/%ED%94%84%EB%A6%AC%EB%AF%B8%EC%96%B4%EB%A6%AC%EA%B7%B8/%EC%88%9C%EC%9C%84/2kwbbcootiqqgmrzs6o5inle5'
 
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url, headers=headers)
 
-site = ''
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    team_list = []
+    teams = soup.select(
+        'body > div.page-container > div.page-container-bg > div.mr-gutters.main-container.clearfix > div > div > div.widget-match-standings > div.widget-match-standings__table-container > table > tbody > tr')
+    for team in teams:
+        # 팀이름
+        teamname = team.select_one('td.widget-match-standings__team > a.widget-match-standings__link > span').text
+        # 팀순위
+        teamrank = team.select_one('td').text
+        # 팀로고
+        teamlogo = team.select_one('td.widget-match-standings__team > a:nth-child(1) > img')['src']
+        # 팀경기수
+        teamp = team.select_one('td.widget-match-standings__matches-played').text
+        # 팀승리
+        teamw = team.select_one('td.widget-match-standings__matches-won').text
+        # 팀무승부
+        teamd = team.select_one('td.widget-match-standings__matches-drawn').text
+        # 팀패배
+        teaml = team.select_one('td.widget-match-standings__matches-lost').text
+        # 팀링크
+        teamlink = 'https://www.goal.com' + team.select_one('td.widget-match-standings__team > a.widget-match-standings__link')['href']
+        team_list.append([teamrank, teamlogo, teamname, teamp, teamw, teamd, teaml, teamlink])  # 리스트로묶음
+    return team_list
+
+#----------메인페이지 result값으로 eplinfo()함수를 받아와서 넘겨줌
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        return render_template('index.html', result=eplinfo(), zip=zip)
+
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
 @app.route('/detail/<int:detail_id>')
 def team_detail(detail_id, site = ''):
+    team = eplinfo()
+    # site_list = []
+    # for teams in team:
+    #     site = teams[7]
+    #     site_list.append([str(site)])
+    site_list = [team[0][7],
+                 team[1][7],
+                 team[2][7],
+                 team[3][7],
+                 team[4][7],
+                 team[5][7],
+                 team[6][7],
+                 team[7][7],
+                 team[8][7],
+                 team[9][7],
+                 team[10][7],
+                 team[11][7],
+                 team[12][7],
+                 team[13][7],
+                 team[14][7],
+                 team[15][7],
+                 team[16][7],
+                 team[17][7],
+                 team[18][7],
+                 team[19][7]]
+    site = ''
     # detail/id 별로 site url 다르게 하기
     detailid = detail_id
     if detailid == 1:
@@ -271,42 +320,6 @@ def update_like():
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
-
-# ------------크롤링
-@app.route('/')
-def home():
-    # ------------크롤링
-    url = 'https://www.goal.com/kr/%ED%94%84%EB%A6%AC%EB%AF%B8%EC%96%B4%EB%A6%AC%EA%B7%B8/%EC%88%9C%EC%9C%84/2kwbbcootiqqgmrzs6o5inle5'
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url, headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    team_list = []
-    teams = soup.select(
-        'body > div.page-container > div.page-container-bg > div.mr-gutters.main-container.clearfix > div > div > div.widget-match-standings > div.widget-match-standings__table-container > table > tbody > tr')
-    for team in teams:
-
-        teamname = team.select_one('td.widget-match-standings__team > a.widget-match-standings__link > span').text  #팀이름
-        teamrank = team.select_one('td').text                                                                       #팀순위
-        teamlogo = team.select_one('td.widget-match-standings__team > a:nth-child(1) > img')['src']                 #팀로고
-        teamp = team.select_one('td.widget-match-standings__matches-played').text                                   #경기수
-        teamw = team.select_one('td.widget-match-standings__matches-won').text                                      #승리
-        teamd = team.select_one('td.widget-match-standings__matches-drawn').text                                    #무승부
-        teaml = team.select_one('td.widget-match-standings__matches-lost').text                                     #패배
-        team_list.append([teamrank, teamlogo, teamname, teamp, teamw, teamd, teaml])                                #리스트로묶음
-#-----------list로만들어서 index페이지로 주기
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        return render_template('index.html', result=team_list, zip = zip)
-
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 #------------닉네임표시
 @app.route('/api/nickname', methods=['GET'])
