84bc13bc524f7df7be1b76249d7d968777e5f96a
nerdved <wjdtjsah12@gmail.com>
2022-01-13 14:56:51 +0900

    app.py - mongo client 주석추가 login.html - Sweetarlert test, 작은 로고 추가

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-
+# client = MongoClient('mongodb://test:test@localhost', 27017)
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
